46177f8f209da660ca388ba087b746a985f4e755
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-22 00:49:46 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,7 @@ def reformat_languages(languages)
   out ={}
   languages.each{|style,info|
     info.each{|language,type|
+        puts style
         if out.has_key?(language)
           out[language][:style].push(style)
         else
